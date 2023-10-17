0c60ba96704ec8832e088920c2f44643379aca79
paabloomaartinez <pablo121002@gmail.com>
2023-10-17 17:07:43 +0200

    Organización general

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/vod', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'vod.html'));
+});
+
+app.get('/live', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'live.html'));
+});
+
+app.listen(port, () => {
+  console.log(`La aplicación está escuchando en http://localhost:${port}`);
+});

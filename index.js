56666f469341586066a232847cbc43a41e0afe19
jonortega <jon.ortega.g@gmail.com>
2023-10-24 11:54:40 +0200

    Intentando hacer live

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const { spawn } = require('child_process');
+const ffmpeg = spawn('ffmpeg', [
+    '-f', 'dshow', // Fuente de video en Windows
+    '-i', 'video="13d3:56b2"', // Especifica la fuente de video (puede variar)
+    // '-i', 'video="OBS Virtual Camera"', // Especifica la fuente de video (puede variar)
+    '-c:v', 'libx264', // Codec de video
+    '-preset', 'ultrafast', // Opciones de codificación
+    '-tune', 'zerolatency',
+    '-f', 'flv', // Formato de salida
+    'http://localhost:3000/live/streamKey', // URL de transmisión
+]);
+
+ffmpeg.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+
+ffmpeg.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+});
+
+ffmpeg.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
@@ -15,12 +39,12 @@ app.get('/vod', (req, res) => {
 
 app.get('/vod/v1', (req, res) => {
     const videoPath = './media/videos/v1.mp4'
-    res.sendFile(videoPath, {root: __dirname })
+    res.sendFile(videoPath, { root: __dirname })
 })
 
 app.get('/vod/i1', (req, res) => {
     const videoPath = './media/images/i1.jpg'
-    res.sendFile(videoPath, {root: __dirname })
+    res.sendFile(videoPath, { root: __dirname })
 })
 
 app.get('/live', (req, res) => {
@@ -28,5 +52,5 @@ app.get('/live', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`La aplicación está escuchando en http://localhost:${port}`);
+    console.log(`La aplicación está escuchando en http://localhost:${port}`);
 });

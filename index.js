7006c610854f574c289d4f144321c47bfe598265
jonortega <jon.ortega.g@gmail.com>
2023-11-25 21:22:02 +0100

    Intentando hacer rtmp-hls manual

--- index.js
+++ index.js
@@ -1,33 +1,77 @@
 const express = require('express');
+// const HLSServer = require('hls-server');
+// const http = require('http');
+const ffmpeg = require('fluent-ffmpeg');
 const path = require('path');
 const app = express();
 const port = 3000;
 
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+    rtmp: {
+        port: 1935,
+        chunk_size: 60000,
+        gop_cache: true,
+        ping: 60,
+        ping_timeout: 30,
+    },
+};
+
+const nms = new NodeMediaServer(config);
+
+// nms.on('postPublish', (id, streamPath, args) => {
+//     // Transcodificación de RTMP a HLS usando ffmpeg
+//     const hlsStreamPath = `hls${streamPath}`;
+//     console.log("Este es el hlsStreamPath: ", hlsStreamPath)
+//     console.log("Este es el streamPath: ", streamPath)
+
+//     ffmpeg(`rtmp://localhost:1935${streamPath}`)
+//         .output(`http://localhost:3000/${hlsStreamPath}/index.m3u8`)
+//         .outputFormat('hls')
+//         .on('end', () => console.log(`Transcodificación completa: ${hlsStreamPath}`))
+//         .on('error', (err) => console.error(`Error en la transcodificación: ${err.message}`))
+//         .run();
+// });
+
+nms.run();
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*const { spawn } = require('child_process');
-const ffmpeg = spawn('ffmpeg', [
-    '-f', 'dshow', // Fuente de video en Windows
-    //'-i', 'video="13d3:56b2"', // Especifica la fuente de video (puede variar)
-    // '-i', 'video="OBS Virtual Camera"', // Especifica la fuente de video (puede variar)
-    '-c:v', 'libx264', // Codec de video
-    '-preset', 'ultrafast', // Opciones de codificación
-    '-tune', 'zerolatency',
-    '-f', 'flv', // Formato de salida
-    'http://localhost:3000/live/streamKey', // URL de transmisión
-]);
+app.get('/hls/:streamName/index.m3u8', (req, res) => {
 
-ffmpeg.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
+    console.log("HA ENTRADO EN EL GET")
+
+    const streamName = req.params.streamName;
+    const hlsStreamPath = `hls/${streamName}`;
+
+    // Configurar cabeceras HTTP para el flujo HLS
+    res.writeHead(200, {
+        'Content-Type': 'application/vnd.apple.mpegurl',
+        'Cache-Control': 'no-cache',
+        'Access-Control-Allow-Origin': '*',
+    });
+
+
+
+    // Ejecutar FFmpeg para generar el archivo M3U8 usando fluent-ffmpeg
+    ffmpeg(`rtmp://localhost:1935/${streamName}`)
+        .output(`${hlsStreamPath}/index.m3u8`)
+        .outputFormat('hls')
+        .on('error', (err) => {
+            console.error(`Error en la transcodificación: ${err.message}`);
+            res.status(500).send('Error en la transcodificación');
+        })
+        .pipe(res, { end: true });
 });
 
-ffmpeg.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-});
+// Intento de hacerlo con HLS
+// const server = http.createServer(app);
 
-ffmpeg.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-});*/
+// const hls = new HLSServer(server, {
+//     path: '/live', // Ruta para acceder al stream HLS
+//     dir: path.join(__dirname, 'streams'), // Directorio donde se guardarán los segmentos de video
+// });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -63,7 +107,7 @@ app.get('/adaptative/v1', (req, res) => {
 app.get('/adaptative/:segment', (req, res) => {
     const segmentNumber = req.params.segment;
     const segmentPath = `./media/dash/${segmentNumber}`;
-    
+
     res.sendFile(segmentPath, { root: __dirname });
 });
 
